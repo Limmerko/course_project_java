00a8ceeca3c393a3470d0361f21ec7f07ac70e93
Vlad <vlmarchenok@mail.ru>
2020-05-05 14:00:07 +0300

    JSP страницы и один контроллер как пример.

--- src/main/java/vlsu/pri117/mep/web/rest/RouteController.java
+++ src/main/java/vlsu/pri117/mep/web/rest/RouteController.java
@@ -3,9 +3,8 @@ package vlsu.pri117.mep.web.rest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import vlsu.pri117.mep.model.News;
-import vlsu.pri117.mep.model.User;
-import vlsu.pri117.mep.model.enums.Roles;
 import vlsu.pri117.mep.service.NewsService;
+import vlsu.pri117.mep.service.impl.NewsServiceImpl;
 
 
 @Controller
@@ -13,34 +12,37 @@ public class RouteController {
 
     private final NewsService newsService;
 
-    public RouteController(NewsService newsService) {
+    public RouteController(NewsService newsService, NewsServiceImpl newsServiceimpl) {
         this.newsService = newsService;
     }
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index() {
-        News news = new News();
-        newsService.save(news);
-        return "/index";
+        // тута сервис
+        return "main";
     }
 
     @GetMapping("/news")
     public String news() {
-        return "/news";
+        // тута сервис
+        return "news";
     }
 
     @GetMapping("/problem-info")
     public String problemInfo() {
+        // тута сервис
         return "problemInfo";
     }
 
     @GetMapping("/problems")
     public String problems() {
+        // тута сервис
         return "problems";
     }
 
     @GetMapping("/report-problem")
     public String reportProblem() {
+        // тута сервис
         return "reportProblem";
     }
 

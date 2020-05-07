47bfec7a2c8dcc59bf2a08a99bab00a5650066e3
Dmitry <unknow>
2020-05-07 20:16:15 +0300

    dlya vlada

--- src/main/java/vlsu/pri117/mep/service/impl/ProblemServiceImpl.java
+++ src/main/java/vlsu/pri117/mep/service/impl/ProblemServiceImpl.java
@@ -2,29 +2,37 @@ package vlsu.pri117.mep.service.impl;
 
 import org.springframework.stereotype.Service;
 import vlsu.pri117.mep.model.Problem;
+import vlsu.pri117.mep.repository.ProblemRepository;
 import vlsu.pri117.mep.service.ProblemService;
 
 import java.util.List;
 
 @Service
 public class ProblemServiceImpl implements ProblemService {
+
+    private final ProblemRepository problemRepository;
+
+    public ProblemServiceImpl( ProblemRepository problemRepository1) {
+        this.problemRepository = problemRepository1;
+    }
+
     @Override
     public Problem save(Problem problem) {
-        return null;
+        return problemRepository.save(problem);
     }
 
     @Override
     public List<Problem> findAll() {
-        return null;
+        return (List<Problem>)problemRepository.findAll();
     }
 
     @Override
     public Problem findOne(Long id) {
-        return null;
+        return problemRepository.findById(id).get();
     }
 
     @Override
     public void delete(Long id) {
-
+        problemRepository.deleteById(id);
     }
 }

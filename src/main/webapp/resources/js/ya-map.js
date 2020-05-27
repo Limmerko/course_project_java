19b3a91ab2603ff26d9fd1edf04958289cf51ee9
Alisa <deprogression@gmail.com>
2020-05-27 15:10:33 +0300

    Починена дата в балуне

--- src/main/webapp/resources/js/ya-map.js
+++ src/main/webapp/resources/js/ya-map.js
@@ -7,6 +7,11 @@ var problemsStatus = document.getElementsByName('problemsStatus');
 var problemsPhoto = document.getElementsByName('problemsPhoto');
 var problemsDate = document.getElementsByName('problemsDate');
 
+var options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+console.log(today.toLocaleDateString("en-US")); // 9/17/2016
+console.log(today.toLocaleDateString("en-US", options));
+
 function init() {
     myMap = new ymaps.Map('map', {
         center: [56.14656449, 40.40288382],
@@ -18,9 +23,6 @@ function init() {
     for (let i=0; i < problemsCoords.length; i++) {
         let details = "/problems/" + problemsId[i].value;
         let now = new Date(problemsDate[i].value);
-        console.log(details);
-        console.log(problemsStatus);
-        console.log("Date: " + now);
         // Создаем геообъект с типом геометрии "Точка".
         myGeoObject = new ymaps.GeoObject({
             // Описание геометрии.
@@ -31,7 +33,7 @@ function init() {
             properties: {
                 balloonContent: '<div><p><img src='+problemsPhoto[i].value+' style=" width="100px;" height="100px" "/></p></div>' +
                                 '<div><p>Статус: '+problemsStatus[i].value+'</p></div>' +
-                                '<div><p>Отправлено: '+now+'</p></div>' +
+                                '<div><p>Отправлено: '+now.toLocaleDateString("ru-RU", options)+'</p></div>' +
                                 '<div><p><a href="'+details+'">Подробнее</a></p></div>'
             }
         });
